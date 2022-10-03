1feaa1fb93177bc485d1823530730b7aa45fd41e
Gera3991 <geraldine.mendieta@gmail.com>
2022-10-02 23:08:26 -0300

    def.variables

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import validator from './validator.js';
 
 console.log(validator);
+
+const input = document.getElementById("number-card-input");
+const boton = document.getElementById("boton-validar");
