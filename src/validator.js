a28eb383a3d4230b3879aab0ba5c7d69cfea4d98
Gera3991 <geraldine.mendieta@gmail.com>
2022-10-02 23:12:42 -0300

    Reverse listo

--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,8 @@
 const validator = {
-  // ...
+isValid : function(NumberCardinput) {
+  const reverseInput = NumberCardinput.tostring().split('').reverse().map(Number);
+  console.log(reverseInput)
+}
 };
 
 export default validator;
