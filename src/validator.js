dfa65a7040181a6b2d29bbb38732dda4b88b17b5
Gera3991 <114420263+Gera3991@users.noreply.github.com>
2022-10-19 23:59:49 -0300

    Update validator.js
    comentarios para guía

--- src/validator.js
+++ src/validator.js
@@ -9,7 +9,7 @@ const validator = {
     }
 
     try {
-      var sumTable = [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 2, 4, 6, 8, 1, 3, 5, 7, 9]];
+      var sumTable = [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 2, 4, 6, 8, 1, 3, 5, 7, 9]]; // Este algoritmo es simple. Obtenemos el reverso del número a verificar (que solamente contiene dígitos [0-9])
       var sum = 0, flip = 0;
 
       for (var i = creditCardNumber.length - 1; i >= 0; i--, flip++) {
