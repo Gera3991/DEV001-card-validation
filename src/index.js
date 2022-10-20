45733e88ccb0f342cbc86d8f31f014d52b2dd847
Gera3991 <114420263+Gera3991@users.noreply.github.com>
2022-10-20 00:52:14 -0300

    Update index.js
    último comentario guía

--- src/index.js
+++ src/index.js
@@ -1,24 +1,29 @@
 import validator from './validator.js';
 
-//Se añade evento de click para confirmr 
-document.getElementById("submitcard").addEventListener("click", creditCardNumber);
+//Cuando se hace click en el botón me traigo los números de la cc-number.
+document.getElementById("submitcard").addEventListener("click", creditCardNumber); 
 
-//Función para atrapar el numero de tarjeta que incluye el algoritmo de luhn
+
+//Los número del que me traje los voy a pasar por esta función.
 function creditCardNumber() {
+  
+  //Primera variable es, sí se escribe algo que no sea un número remplaza el espacio por nada.
   let creditCardNumber = document.getElementById("cc-number").value.replace(/\D/g, ''); 
 
+  //Segunda variable sirve para enmascarar número de la tarjeta de crédito con # .
   let cc = validator.maskify(creditCardNumber);
-
-  let luhn = validator.isValid(creditCardNumber);
-
+  
+  // Tercera variable es para ejecutar el algoritmo de luhn
+  let luhn = validator.isValid(creditCardNumber); 
+  
+// Después me traigo los digitos de la tarjeta de crédito con las varibles ejecutadas.
   document.getElementById("cc-number").value = cc;
 
+// Utilizó un boolean para validar si es true o false mi tarjeta de credito.
   if (luhn){
     console.log("Tarjeta valida");
     alert("Tarjeta valida")
     
-   
-
   } else {
     console.log("Tarjeta invalida");
         alert("Tarjeta invalida")
